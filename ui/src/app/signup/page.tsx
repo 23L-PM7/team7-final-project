33348a06fcbb2476c987178180a0143321226d17
Derrick Munkhbat <106757077+derrick-munkhbat@users.noreply.github.com>
2024-04-29 18:56:22 +0800

    fixed folders

---
+++ ui/src/app/signup/page.tsx
@@ -0,0 +1,35 @@
+export default function signUp() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex">
+        <button>X</button>
+        <h1>Log in or sign up</h1>
+      </div>
+      <div>
+        <h1>Welcome to Airbnb</h1>
+      </div>
+      <div>
+        <div>
+          <p>Country code</p>
+          <p>Mongolia (976)</p>
+        </div>
+        <div className="dropdown dropdown-right dropdown-end">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Click
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
