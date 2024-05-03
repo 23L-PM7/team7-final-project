1559e97e0e83a37233f3032e1bb27f5fa0eb4639
Derrick Munkhbat <23LP8019@nest.edu.mn>
2024-05-03 18:31:19 +0800

    footer added

---
+++ ui/src/components/footer/Footer.tsx
@@ -0,0 +1,35 @@
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { RiGlobalLine } from "react-icons/ri";
+
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-0 w-full bg-slate-100">
+      <div className=" mx-auto w-[1280px] h-[69px] flex justify-between items-center px-40">
+        <div className="flex">
+          <h3 className="mr-5">© 2024 Airbnb, Inc.</h3>
+          <ul className="flex justify-evenly gap-5">
+            <li>Terms</li>
+            <li>Sitemap</li>
+            <li>Privacy</li>
+            <li>Your Privacy Choices</li>
+          </ul>
+        </div>
+
+        <div className="flex justify-between gap-5 font-bold">
+          <button className="flex hover:underline">
+            <RiGlobalLine className="w-6 h-6" />
+            <h3>English (US)</h3>
+          </button>/œ
+          <button className="hover:underline">$ USD</button>
+          <FaSquareFacebook className="w-6 h-6" />
+          <FaSquareTwitter className="w-6 h-6" />
+          <FaSquareInstagram className="w-6 h-6" />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
