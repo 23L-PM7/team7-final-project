cd8975d315b24c856651d3ee6d05cac1e71c02b6
Амгаланбаатар Галхүү <23LP5157@admins-iMac.local>
2024-05-03 19:06:20 +0800

    card

--- ui/src/components/header/Navbar.tsx
+++ ui/src/components/header/Navbar.tsx
@@ -3,22 +3,20 @@ import Search from "./Search";
 import User from "./User";
 import Container from "../Container";
 
-
 const NavBar = () => {
-    return (
-      <header className="fixed w-full bg-white z-10 shadow-sm">
-        <div className="py-4 border-b-[1px]">
-          <Container>
-            <div className="flex items-center justify-between gap-3 md:gap-0">
-              <Logo />
-              <Search />
-              <User />
-            </div>
-          </Container>
-        </div>
-      </header>
-    );
-  };
-  
-  export default NavBar;
-  
+  return (
+    <header className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex mx-auto container items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <User />
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
--- ui/src/components/homePageCard/HeartButton.tsx
+++ ui/src/components/homePageCard/HeartButton.tsx
@@ -1,18 +1,3 @@
-// import { FaRegHeart } from "react-icons/fa";
-// export function HeartButton() {
-//   function toggleFavorite() {
-//     return;
-//   }
-//   return (
-//     <div
-//       onClick={toggleFavorite}
-//       className="text-white text-xl relative hover:opacity-80 transition cursor-pointer"
-//     >
-//       <FaRegHeart />
-//     </div>
-//   );
-// }
-
 import * as React from "react";
 import Button from "@mui/joy/Button";
 import Modal from "@mui/joy/Modal";
