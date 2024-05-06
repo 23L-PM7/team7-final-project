4de5d52c5e45513ad2abb1e907934d3aea42b9f7
Амгаланбаатар Галхүү <23LP5157@admins-iMac.local>
2024-05-06 20:32:49 +0800

    merge solved

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { HomePageCards } from "../components/homePageCard/HomePageCard";
-=======
 import { HomePageCards } from "./components/homePageCard/HomePageCard";
->>>>>>> 0d3c6d3 (heart brn modal)
 
 export default function Home() {
   return (
