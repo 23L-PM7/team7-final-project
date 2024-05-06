da0815e9cfdbf80a147b33180c6b61e0fc47065b
hi <hihi@his-Laptop.local>
2024-05-06 16:49:54 +0800

    listing

---
+++ ui/src/app/api/listing/route.ts
@@ -0,0 +1,45 @@
+import { dbRequest } from "../../../utils/dbRequest";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const lists = await dbRequest("listing", "find");
+
+  return Response.json(lists);
+}
+
+export async function POST(request: Request) {
+  const listData = await request.json();
+
+  const createList = await dbRequest("listing", "insertOne", {
+    document: {
+      ...listData,
+    },
+  });
+
+  return Response.json(createList);
+}
+
+// export async function PUT(request: Request) {
+//   const data = await dbRequest("listing", "updateOne", {
+//     filter: { id: {} },
+//     update: {
+//       set: {
+//         id: "",
+//         userId: "",
+//         title: "",
+//         description: "",
+//         imageSrc: "",
+//         createdAt: "",
+//         category: "",
+//         roomCount: "",
+//         bathroom: "",
+//         guestCount: "",
+//         childrenCount: "",
+//         locationValue: "",
+//         price: "",
+//       },
+//     },
+//   });
+
+//   return Response.json(data);
+// }
