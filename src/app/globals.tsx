dfccafc8925498fae9f69cf4db614e19ffb748b0
ganaa0419 <b.ganaa0419@gmail.com>
2024-05-17 05:58:53 +0800

    counter value set

--- src/app/globals.tsx
+++ src/app/globals.tsx
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import dayjs, { Dayjs } from "dayjs";
+import dayjs from "dayjs";
 
 export const useDate = create((set) => ({
   date: [dayjs("2022-05-15"), dayjs("2022-05-15")],
@@ -15,19 +15,23 @@ export const useGuests = create((set) => ({
   clientNumber: 0,
   setClientNumber: (newNumber: Number) => set(() => ({ clientNumber: newNumber })),
 }));
-// export const useAdult = create((set) => ({
-//   adultNumber: 0,
-//   setClientNumber: (newNumber: Number) => set(() => ({ clientNumber: newNumber })),
-// }));
-// export const useChildren = create((set) => ({
-//   childrenNumber: 0,
-//   setClientNumber: (newNumber: Number) => set(() => ({ clientNumber: newNumber })),
-// }));
-// export const useInfant = create((set) => ({
-//   infantNumber: 0,
-//   setClientNumber: (newNumber: Number) => set(() => ({ clientNumber: newNumber })),
-// }));
-// export const usePet = create((set) => ({
-//   petNumber: 0,
-//   setClientNumber: (newNumber: Number) => set(() => ({ clientNumber: newNumber })),
-// }));
+
+export const useAdult = create((set) => ({
+  adultNumber: 0,
+  setAdultNumber: (newNumber: Number) => set(() => ({ adultNumber: newNumber })),
+}));
+
+export const useChildren = create((set) => ({
+  childrenNumber: 0,
+  setChildrenNumber: (newNumber: Number) => set(() => ({ childrenNumber: newNumber })),
+}));
+
+export const useInfant = create((set) => ({
+  infantNumber: 0,
+  setInfantNumber: (newNumber: Number) => set(() => ({ infantNumber: newNumber })),
+}));
+
+export const usePet = create((set) => ({
+  petNumber: 0,
+  setPetNumber: (newNumber: Number) => set(() => ({ petNumber: newNumber })),
+}));
