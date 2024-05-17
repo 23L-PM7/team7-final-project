8e059546a3ddf9d0bad91fd7fbef935dd2c326ce
ganaa1359 <23lp2023@nest.edu.mn>
2024-05-17 18:09:15 +0800

    push router with ID is done

--- src/app/rooms/[listingId]/ListingIntroduction.tsx
+++ src/app/rooms/[listingId]/ListingIntroduction.tsx
@@ -132,13 +132,9 @@ export function ListingIntroduction() {
             </LocalizationProvider> */}
         </div>
       </div>
-<<<<<<< HEAD
       <div className="w-2/6">
         <ListingReservation />
       </div>
-=======
-      <ListingReservation />
->>>>>>> dfccafc8925498fae9f69cf4db614e19ffb748b0
     </div>
   );
 }
