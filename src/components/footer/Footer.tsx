9c011f87d916dece4ad9435bb8b63332614d2448
ganaa1359 <23lp2023@nest.edu.mn>
2024-05-21 17:40:11 +0800

    some bugs fixed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -18,37 +18,36 @@ const Footer = () => {
 
         </div>
 
-        </div>
+      </div>
 
-        <div className="relative">
-          <div className="fixed left-0 right-0 bottom-0 bg-slate-100 z-10">
-            <div className=" mx-auto w-[1280px] h-[69px] flex justify-between items-center px-40">
-              <div className="flex">
-                <h3 className="mr-5">© 2024 Airbnb, Inc.</h3>
-                <ul className="flex justify-evenly gap-5">
-                  <li>Terms</li>
-                  <li>Sitemap</li>
-                  <li>Privacy</li>
-                  <li>Your Privacy Choices</li>
-                </ul>
-              </div>
+      <div className="relative">
+        <div className="fixed left-0 right-0 bottom-0 bg-slate-100 z-10">
+          <div className=" mx-auto w-[1280px] h-[69px] flex justify-between items-center px-40">
+            <div className="flex">
+              <h3 className="mr-5">© 2024 Airbnb, Inc.</h3>
+              <ul className="flex justify-evenly gap-5">
+                <li>Terms</li>
+                <li>Sitemap</li>
+                <li>Privacy</li>
+                <li>Your Privacy Choices</li>
+              </ul>
+            </div>
 
-              <div className="flex justify-between gap-5 font-bold">
-                <button className="flex hover:underline">
-                  <RiGlobalLine className="w-6 h-6" />
-                  <h3>English (US)</h3>
-                </button>
-                <button className="hover:underline">$ USD</button>
-                <FaSquareFacebook className="w-6 h-6" />
-                <FaSquareTwitter className="w-6 h-6" />
-                <FaSquareInstagram className="w-6 h-6" />
-              </div>
+            <div className="flex justify-between gap-5 font-bold">
+              <button className="flex hover:underline">
+                <RiGlobalLine className="w-6 h-6" />
+                <h3>English (US)</h3>
+              </button>
+              <button className="hover:underline">$ USD</button>
+              <FaSquareFacebook className="w-6 h-6" />
+              <FaSquareTwitter className="w-6 h-6" />
+              <FaSquareInstagram className="w-6 h-6" />
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 };
 
 export default Footer;
