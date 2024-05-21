8ec46baa50d4d3d259d4f34921ed1a649a6c9726
Amgalanbaatar888 <23lp5157@nest.edu.mn>
2024-05-21 16:48:12 +0800

    hi

--- src/components/newListHeaderFooter/ProgressFooter.tsx
+++ src/components/newListHeaderFooter/ProgressFooter.tsx
@@ -3,9 +3,12 @@
 import * as React from "react";
 import Stack from "@mui/joy/Stack";
 import LinearProgress from "@mui/joy/LinearProgress";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { create } from "zustand";
+import axios from "axios";
+import { useBecomeHost } from "@/app/become-a-host/store";
+import path from "path";
 
 export const useNextButtonClickable = create((set) => ({
   nextButtonClickable: false,
@@ -32,6 +35,48 @@ const stepValues = [
 ];
 
 export function ProgressFooter() {
+  const {
+    title,
+    price,
+    image,
+    image1,
+    image2,
+    image3,
+    image4,
+    bedroomCount,
+    bathroomCount,
+    bedCount,
+    guestsCount,
+    description,
+    offerTypes,
+    roomType,
+    type,
+    location,
+    region,
+  } = useBecomeHost();
+
+  const addListing = async () => {
+    try {
+      const response = await axios.post("/api/listing", {
+        title,
+        price,
+        images: [image, image1, image2, image3, image4],
+        bedroomCount,
+        bathroomCount,
+        guestsCount,
+        bedCount,
+        description,
+        offerTypes,
+        roomType,
+        type,
+        location,
+        region,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const pathname = usePathname();
   const router = useRouter();
 
@@ -43,9 +88,20 @@ export function ProgressFooter() {
   const { nextButtonClickable, setNextButtonClickable }: any =
     useNextButtonClickable();
 
-  if (pathname === undefined) {
-    router.push("/overview");
-  }
+  const checkBack = () => {
+    stepValues[index - 1];
+    console.log(pathname);
+    if (pathname === "/become-a-host/about-your-place") {
+      router.push("/overview");
+    }
+  };
+
+  const combined = () => {
+    router.push(`/become-a-host/${stepValues[index + 1]}`);
+    if (stepValues[index] == "receipt") {
+      addListing();
+    }
+  };
 
   return (
     <div className="relative">
@@ -57,18 +113,16 @@ export function ProgressFooter() {
           ></div>
         </div>
         <div className="flex sticky bottom-0 justify-between pl-10 p-4 pr-10">
-          <a
-            href={`/become-a-host/${stepValues[index - 1]}`}
+          <button
+            onClick={checkBack}
             className="font-semibold rounded flex justify-center  items-center hover:bg-slate-200 h-8 p-2"
           >
             Back
-          </a>
+          </button>
           <span>{pathname}</span>
           <button
             disabled={!nextButtonClickable}
-            onClick={() =>
-              router.push(`/become-a-host/${stepValues[index + 1]}`)
-            }
+            onClick={() => combined()}
             className="w-[100px] h-[48px] border p-2 flex items-center justify-center rounded-md text-white bg-gray-700 hover:bg-black disabled:opacity-50 disabled:cursor-not-allowed"
           >
             Next
