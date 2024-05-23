95a4a0415f44c12e00c8dcad9650f379cead065d
Мөнхдөлгөөн Munkhbat <23LP8019@Admins-iMac.local>
2024-05-23 18:05:52 +0800

    conflict fixed

--- src/components/LittleMenus/Calendar.tsx
+++ src/components/LittleMenus/Calendar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 import * as React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers-pro";
@@ -20,37 +19,6 @@ const Calendar = () => {
     (name: string, value: string) => {
       const params = new URLSearchParams(searchParams.toString());
       params.set(name, value);
-=======
-"use client"
-import * as React from 'react';
-import { DateRange } from '@mui/x-date-pickers-pro/models';
-import { LocalizationProvider, monthCalendarClasses } from '@mui/x-date-pickers-pro';
-import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
-import dayjs, { Dayjs } from 'dayjs';
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
-// import { useRouter } from 'next/router';
-import { usePathname, useSearchParams, useRouter } from 'next/navigation';
-import { useDate, useDays } from '../../app/globals';
-
-const Calendar = () => {
-    const router = useRouter()
-    const pathname = usePathname()
-    const searchParams = useSearchParams()
-    const { date, setDate }: any = useDate();
-    const { daysNumber, setDaysNumber } : any = useDays();
-    const search = searchParams.get('date')?.toString
-
-    const totalDays = 0 - date[0].diff(date[1], "day")
-
-    useEffect(() => {
-        setDaysNumber(totalDays);
-      }, [totalDays]);
-
-      console.log(daysNumber);
->>>>>>> main
 
       return params.toString();
     },
@@ -59,7 +27,6 @@ const Calendar = () => {
 
   console.log(date);
 
-<<<<<<< HEAD
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={["DateRangeCalendar"]}>
@@ -74,15 +41,6 @@ const Calendar = () => {
           ok
         </Link>
       </DemoContainer>
-=======
-    return <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={['DateRangeCalendar']}>
-            <DateRangeCalendar
-                value={date}
-                onChange={(newDate) => setDate(newDate)} />
-            <Link href={pathname + '?' + createQueryString('date', date.toString())}></Link>
-        </DemoContainer>
->>>>>>> main
     </LocalizationProvider>
   );
 };
