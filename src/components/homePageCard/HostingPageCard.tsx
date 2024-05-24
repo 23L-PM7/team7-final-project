49809685ce7e90954c4ec32f677d67848a3248a4
Amgalanbaatar888 <23lp5157@nest.edu.mn>
2024-05-24 15:01:36 +0800

    hi

--- src/components/homePageCard/HostingPageCard.tsx
+++ src/components/homePageCard/HostingPageCard.tsx
@@ -20,7 +20,6 @@ export function HostingPageCard({ size }: any) {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
   const searchParams = useSearchParams();
-
   const categoriesFilter = searchParams.get("categories");
 
   function fetchCards() {
@@ -48,63 +47,71 @@ export function HostingPageCard({ size }: any) {
     return card.type === categoriesFilter;
   });
   // if (loading) return <Loading />;
+
   return (
     <>
       <div className="flex">
         <div className="flex gap-[73px]">
-          {filteredCards.map((card: any, index) => (
-            <div key={card._id} className="flex flex-col relative">
-              <Swiper
-                cssMode={true}
-                navigation={true}
-                mousewheel={true}
-                keyboard={true}
-                pagination={true}
-                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                // onSwiper={(swiper) => console.log(swiper)}
-                className="h-[250px] w-[250px] rounded-lg !mx-0 "
-              >
-                {card.images
-                  .filter((image: string) => image)
-                  .map((image: any, index: any) => (
-                    <SwiperSlide key={index}>
-                      <div className="aspect-square w-full flex justify-start items-start  overflow-hidden rounded-xl">
-                        <img
-                          src={image}
-                          alt=""
-                          onClick={() => pushToListing(card._id)}
-                          className={`w-full flex items-center max-w-xs transition cursor-pointer duration-300 ease-in-out hover:scale-110 h-full object-cove`}
-                        />
-                      </div>
-                    </SwiperSlide>
-                  ))}
-              </Swiper>
+          {filteredCards
+            .slice(0)
+            .toReversed()
+            .filter((_: any, index) => index < 5)
+            .map((card: any, index) => (
+              <div key={card._id} className="flex flex-col relative">
+                <Swiper
+                  cssMode={true}
+                  navigation={true}
+                  mousewheel={true}
+                  keyboard={true}
+                  pagination={true}
+                  modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                  // onSwiper={(swiper) => console.log(swiper)}
+                  className="h-[250px] w-[250px] rounded-lg !mx-0 "
+                >
+                  {card.images
+                    .filter((image: string) => image)
+                    .map((image: any, index: any) => (
+                      <SwiperSlide key={index}>
+                        <div className="aspect-square w-full flex justify-start items-start  overflow-hidden rounded-xl">
+                          <img
+                            src={image}
+                            alt=""
+                            onClick={() => pushToListing(card._id)}
+                            className={`w-full flex items-center max-w-xs transition cursor-pointer duration-300 ease-in-out hover:scale-110 h-full object-cove`}
+                          />
+                        </div>
+                      </SwiperSlide>
+                    ))}
+                </Swiper>
 
-              <div
-                className="flex flex-col justify-start cursor-pointer items-start "
-                onClick={() => pushToListing(card._id)}
-              >
-                <div className="flex justify-between">
-                  <div className="font-semibold text-lg w-60">{card.title}</div>
-                </div>
+                <div
+                  className="flex flex-col justify-start cursor-pointer items-start "
+                  onClick={() => pushToListing(card._id)}
+                >
+                  <div className="flex justify-between">
+                    <div className="font-semibold text-lg w-60">
+                      {card.title}
+                    </div>
+                  </div>
 
-                {/* <div className="font-light  text-neutral-500 w-[300px]">
+                  {/* <div className="font-light  text-neutral-500 w-[300px]">
                   {card.location}
                 </div> */}
-                {/* <div className="font-light text-neutral-500">{card.region}</div> */}
-                <div className="flex gap-1">
-                  {card.bedroomCount} <p className="text-neutral-500"> beds</p>
-                </div>
-                <div className="font-light  text-neutral-500"></div>
-                <div className="flex gap-3">
-                  <div className="font-medium flex gap-1">
-                    ${card.price} <p className="text-neutral-500">nights</p>
+                  {/* <div className="font-light text-neutral-500">{card.region}</div> */}
+                  <div className="flex gap-1">
+                    {card.bedroomCount}{" "}
+                    <p className="text-neutral-500"> beds</p>
                   </div>
-                  <div className="text-black text-md"></div>
+                  <div className="font-light  text-neutral-500"></div>
+                  <div className="flex gap-3">
+                    <div className="font-medium flex gap-1">
+                      ${card.price} <p className="text-neutral-500">nights</p>
+                    </div>
+                    <div className="text-black text-md"></div>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </>
